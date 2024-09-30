06e7f805db3af2a4933cdae2674eb7104cdd1204
chris2pher1503 <christopher150304@outlook.com>
2024-09-30 17:09:33 +0200

    added challanging task

--- exercise_4/script.py
+++ exercise_4/script.py
@@ -5,6 +5,8 @@ name= "Christopher Ljosland Strand"
 nmbu_email="christopher.ljosland.strand@nmbu.no"
 
 from pathlib import Path
+import numpy as np
+import time
 #cwd is INF201, defined here to make it easier
 cwd = Path.cwd()
 exercise_4_path = cwd / "Exercises" / "exercise_4"
@@ -61,3 +63,48 @@ def generate_files(exercises):
         print(path)
 
 generate_files(create_exercises())
+
+
+#task 2: 
+
+def matrix_vector_product(matrix, vector):
+    result = [0] * len(matrix) 
+    for i in range(len(matrix)):
+        for j in range(len(vector)):
+            result[i] += matrix[i][j] * vector[j]
+    return result
+
+
+def numpy_matrix_vector_product(matrix, vector):
+    result = np.matmul(matrix, vector)
+    
+    return result
+
+
+matrix = [[11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+ [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+ [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+ [41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+ [51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+ [61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+ [71, 72, 73, 74, 75, 76, 77, 78, 79, 80],
+ [81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+ [91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+ [101, 102, 103, 104, 105, 106, 107, 108, 109, 110]]
+
+vector = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+starttime_non_numpy = time.time()
+print(matrix_vector_product(matrix, vector))
+endtime_non_numpy = time.time()
+starttime_numpy = time.time()
+print(numpy_matrix_vector_product(np.array(matrix), np.array(vector)))
+endtime_numpy = time.time()
+difference_non_numpy = endtime_non_numpy - starttime_non_numpy
+difference_numpy = endtime_numpy - starttime_numpy
+
+print(f"Time taken for non-numpy: {difference_non_numpy:.6f}")
+print(f"Time taken for numpy: {difference_numpy:.6f}")
+print(f"{difference_numpy/difference_non_numpy:.2f} times faster with non numpy")
+        
