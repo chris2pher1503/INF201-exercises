c8cc457510bc287324289803c1367f086467a421
chris2pher1503 <christopher150304@outlook.com>
2024-09-30 15:52:17 +0200

    added warm up task

--- exercise_4/script.py
+++ exercise_4/script.py
@@ -5,13 +5,30 @@ name= "Christopher Ljosland Strand"
 nmbu_email="christopher.ljosland.strand@nmbu.no"
 
 #task 0:
-
-
-
-
-
-#task 1:
 from pathlib import Path
+#cwd is INF201, defined here to make it easier
+cwd = Path.cwd()
+exercise_4_path = cwd / "Exercises" / "exercise_4"
+
+
+def create_dir():
+    sub_dirs = ["data", "output"]
+    projects_task0_path = exercise_4_path / "projects_task0"
+    projects_task0_path.mkdir(exist_ok=True)
+    for sub_dir in sub_dirs:
+        sub_dir_path = projects_task0_path / sub_dir
+        if not sub_dir_path.exists():
+            sub_dir_path.mkdir()
+            file_path = sub_dir_path / (sub_dir + ".txt")
+            print(file_path)
+            file_path.touch()    
+             
+                
+        
+
+create_dir()
+#task 1:
+
 
 
 def create_exercises(total_number=4, project_assigments_start=3):
@@ -21,18 +38,16 @@ def create_exercises(total_number=4, project_assigments_start=3):
 
 def generate_files(exercises):
     students = ["Ole", "Sarah"]
-    cwd = Path.cwd()
-    projects_path  = cwd / "Exercises" / "exercise_4" / "projects"
+
+    projects_path  = exercise_4_path / "projects"
     projects_path.mkdir(exist_ok=True)
      
     for exercise in exercises: 
         exercise_path = projects_path / exercise
-        print(f"Creating exercise directory: {exercise_path}")
         exercise_path.mkdir(exist_ok=True)
         
         for student in students:
             student_path = exercise_path / student
-            print(f"Creating student directory: {student_path}")
             student_path.mkdir(exist_ok=True)
 
 
