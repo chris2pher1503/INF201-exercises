a40e9402d711edeaca3866a38967df9602813c13
chris2pher1503 <christopher150304@outlook.com>
2024-10-07 17:57:17 +0200

    fixed task 2

--- exercise_5/script.py
+++ exercise_5/script.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-
+import time
 
 #task 1: 
 matrix = np.random.randint(0, 100, (5000, 5000))
@@ -36,18 +36,54 @@ print("mean of the matrix: ", mean)
 print("variance of the matrix: ",variance)
 print("multiplied of the matrix: ",multiply(matrix, 4))
  
-#task 2: 
+#task 2:
 A = np.zeros((50 , 50))
 print("A: ", A) 
+
+for i in range( len(A)):
+    A[i,i] = -2
+    if i < (len(A) - 1):
+        A[i, i+1] = 1
+    if i > 0:
+        A[i, i-1] = 1
+    
+print("A: \n", A)
+
+
+v = np.random.rand(50)
+v = v / np.linalg.norm(v)
+
+def dominant_eigenvalue(A, v):
+    v_new = np.matmul(A, v)
+    v = v_new / np.linalg.norm(v_new)
+    return v
+
+    
+for i in range(100):
+    v = dominant_eigenvalue(A, v)
+dominant_eigenvalue = (A @ v) @ v / (v @ v)
+print("dominant eigenvalue: ", dominant_eigenvalue)
+
+eigenvalues, eigenvectors = np.linalg.eig(A)
+
+
+abs_eigenvalues = np.abs(eigenvalues)
+dominant_index = np.argmax(abs_eigenvalues)
+
+print("dominant eigenvalue (numpy):", eigenvalues[dominant_index])
+
+
+
+""" 
 A[0][1] = 1
 A[0][0] = -2
-for i in range(1, len(A)):
-    for j in range(1, len(A[i])):
+
+
+for j in range(1, len(A[i])):
         if i == j:
             A[i][j] = -2
             if j < len(A[i]) - 1:
                 A[i][j+1] = 1
             if j < len(A[i]) - 1:
                 A[i][j-1] = 1
-A[49][48] = 1                
-print("A: \n", A)
+A[49][48] = 1  """
