2a5b836f11c5800986b861fe672b7f6e790ec88e
chris2pher1503 <christopher150304@outlook.com>
2024-10-07 14:26:17 +0200

    started on task 5

---
+++ exercise_5/script.py
@@ -0,0 +1,61 @@
+import numpy as np
+import random
+
+matrix = np.random.randint(0, 100, (5000, 5000))
+
+def sum(matrix):
+    sum = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            sum += matrix[i][j]
+    return sum
+
+def mean(matrix, sum):
+    mean = sum / (len(matrix) * len(matrix[0]))
+    return mean
+
+def variance(matrix, mean, sum):
+    variance = sum / (len(matrix) * len(matrix[0]))**2
+    return variance
+
+def multiply(matrix, scalar):
+    return matrix * scalar
+
+print("sum of the matrix: ", sum(matrix))
+print("mean of the matrix: ", mean(matrix, sum(matrix)))
+print("variance of the matrix: ",variance(matrix, mean(matrix, sum(matrix)), sum(matrix)))
+print("multiplied of the matrix: ",multiply(matrix, 4))
+
+
+
+
+
+
+""" def mean(matrix):
+    sum = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])): 
+            sum += int(matrix[i][j])
+    mean_value = sum / (len(matrix) * len(matrix[0]))
+                       
+    return mean_value
+
+def variance(matrix, mean):
+    sum= 0
+    for i in range (len(matrix)):
+        for j in range(len(matrix[i])):
+            sum += (mean - matrix[i][j]) ** 2
+    return sum / (len(matrix) * len(matrix[0]))**2 """
+
+
+
+""" def multiplt(matrix, scalar):
+    return matrix * scalar
+
+
+print(mean(matrix))
+print(variance(matrix))
+print(sum(matrix))
+print(multiplt(matrix, 4))
+
+ """
