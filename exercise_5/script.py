fixed variance

exercise_5/script.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 
+
+#task 1: 
 matrix = np.random.randint(0, 100, (5000, 5000))
 
 def sum(matrix):
@@ -14,8 +16,13 @@ def mean(matrix, sum):
     mean = sum / (len(matrix) * len(matrix[0]))
     return mean
 
-def variance(matrix, mean, sum):
-    variance = sum / (len(matrix))**2
+def variance(matrix, mean):
+    sum = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            sum += (mean - matrix[i][j]) ** 2
+    
+    variance =  sum / (len(matrix))**2
     return variance
 
 def multiply(matrix, scalar):
@@ -23,7 +30,7 @@ def multiply(matrix, scalar):
 
 print("sum of the matrix: ", sum(matrix))
 print("mean of the matrix: ", mean(matrix, sum(matrix)))
-print("variance of the matrix: ",variance(matrix, mean(matrix, sum(matrix)), sum(matrix)))
+print("variance of the matrix: ",variance(matrix, mean(matrix, sum(matrix))))
 print("multiplied of the matrix: ",multiply(matrix, 4))
 
 
@@ -59,3 +66,6 @@ print(sum(matrix))
 print(multiplt(matrix, 4))
 
  """
+ 
+ #task 2: 
+ 